58e9b6dff949e26c33782bf5f849577e7d08b140
vivignutz <vgnutzmann@gmail.comm>
2023-11-22 17:03:30 +0100

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+// Footer.js
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <p>Footer here</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+// Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1>Header here</h1>
+    </div>
+  );
+}
+
+export default Header;
